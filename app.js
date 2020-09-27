2dd275dbe71bdff09f27c54c76c065faf3de5bf5
Raphael Barbosa Marques <raphael.rbm.dev@gmail.com>
2020-09-27 11:15:59 -0300

    Iniciando os estudos em Node.js

---
+++ app.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b
+}
+
+function div(a,b) {
+    return a / b;
+}
+
+let val_1 = 8;
+let val_2 = 10;
+
+console.log("Sum: " + sum(val_1, val_2));
+console.log("Sub: " + sub(val_1, val_2));
+console.log("Mult: " + mult(val_1, val_2));
+console.log("Div: " + div(val_1, val_2));
